d23ad03b72014a161b98a42302a7a3979c53eefb
yousef <youssefnaro@gmail.com>
2022-12-02 18:01:38 +0100

    Get All Question

--- affiche_qst.php
+++ affiche_qst.php
@@ -3,5 +3,4 @@ require('config.php');
 $getAllMyQuestions = $bdd->prepare('SELECT id_user, qst_title, msg FROM question WHERE id_user = ? ORDER BY id_user DESC');
 $getAllMyQuestions->execute(array($_SESSION['id']));
 
-
 ?>
